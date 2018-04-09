a652523fc4e3276fd5eec277a239b77e5d6a379f
fryter <fryter@sina.com>
2018-04-09 14:43:16 +0800

    Initial commit

---
+++ DotNetCoreWebApi/Controller/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Owin;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace DotNetCoreWebApa.Controllers
+{
+   public class TestController:Controller
+    {
+
+        public string test()
+        {
+            return "test";
+        }
+    }
+}
---
+++ DotNetCoreWebApi/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace DotNetCoreWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BuildWebHost(args).Run();
+        }
+        public static IWebHost BuildWebHost(string[] args) =>
+             WebHost.CreateDefaultBuilder(args).UseKestrel().UseUrls("http://*:5000;http://localhost:5001;http://hostname:5002")
+                 .UseStartup<Startup>()
+                 .Build();
+    }
+}
